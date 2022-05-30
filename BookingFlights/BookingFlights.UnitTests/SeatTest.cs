711905055c0272e869d04d419447321b72815018
ionut-buzduga <buzduga.ionutgabriel@gmail.com>
2022-05-30 19:26:47 +0300

    Created Mock tests for seats

--- BookingFlights/BookingFlights.UnitTests/SeatTest.cs
+++ BookingFlights/BookingFlights.UnitTests/SeatTest.cs
@@ -4,6 +4,7 @@ using BookingFlights.DataAccess;
 using BookingFlights.DataModel;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,74 +17,118 @@ namespace BookingFlights.UnitTests
     public class SeatTest
     {
 
-        private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=BookingFlightDb;Trusted_Connection=True";
+        //private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=BookingFlightDb;Trusted_Connection=True";
 
-        //function for intialization of the service
-        private SeatService init()
+        ////function for intialization of the service
+        //private SeatService init()
+        //{
+        //    BookingFlightsDbContext dbContext = new BookingFlightsDbContext(new DbContextOptionsBuilder<BookingFlightsDbContext>()
+        //        .UseSqlServer(ConnectionString)
+        //        .Options);
+        //    IRepositoryWrapper repositoryWrapper = new RepositoryWrapper(dbContext);
+        //    return new SeatService(repositoryWrapper);
+        //}
+
+
+        private Mock<IRepositoryWrapper> seatRepoMock = new Mock<IRepositoryWrapper>();
+
+        private Guid existingSeatId = Guid.Parse("ca8ef5a9-79a8-4a8a-57bc-08da3984ad00");
+        private Guid existingFlightId = Guid.Parse("bdc9b0c2-247d-4701-ead9-08da3984acee");
+       
+
+        [TestInitialize]
+        public void intializeTest()
         {
-            BookingFlightsDbContext dbContext = new BookingFlightsDbContext(new DbContextOptionsBuilder<BookingFlightsDbContext>()
-                .UseSqlServer(ConnectionString)
-                .Options);
-            IRepositoryWrapper repositoryWrapper = new RepositoryWrapper(dbContext);
-            return new SeatService(repositoryWrapper);
+            var seat = new Seat()
+            {
+                Id = existingSeatId,
+                FlightId = existingFlightId,
+               
+               
+            };
+
+            seatRepoMock.Setup(seatRepo => seatRepo.SeatsRepository.GetAll()).Returns(new List<Seat> { seat }.AsQueryable());
         }
 
         [TestMethod]
         public void GetAllSeatsCreated()
         {
-            //Arange
-            SeatService _seatService = init();
-
+            var seatService = new SeatService(seatRepoMock.Object);
             //Act
-            var seatList = _seatService.GetAllQueryable().ToList();
+            var seatList = seatService.GetAllQueryable();
 
             //Assert
-            Assert.AreEqual(75, seatList.Count);
+            Assert.AreEqual(1, seatList.ToList().Count);
         }
 
 
         [TestMethod]
-        public void GetAllSeatsForFlight()
+        public void FindSpecificSeatForFlight()
         {
             //Arrange
-            SeatService _seatService = init();
-            Guid SeatId = Guid.Parse("42fdf6f2-0bff-4ee6-3ec7-08da3968162b");
+            Guid FlightId = Guid.Parse("bdc9b0c2-247d-4701-ead9-08da3984acee");
             
 
-            var findSeat = _seatService.GetByCondition(seat => seat.FlightId == SeatId);
-            
-            foreach (var seat in findSeat)
+            var seatService = new SeatService(seatRepoMock.Object);
+
+            //Act
+            var findSeat = seatService.GetByCondition(seat => seat.FlightId == FlightId);
+
+            foreach (Seat seat in findSeat)
             {
-                Assert.IsNotNull(seat);
+                Assert.AreEqual(existingSeatId, seat.Id);
             }
-
-            Assert.AreEqual(25, findSeat.Count());
         }
 
-        [TestMethod]
-        public void UpdateSpecificSeat()
-        {
-            SeatService seatService = init();
 
-            Guid seatGuid = Guid.Parse("668e7f08-260a-4d8f-1fd9-08da3968163c");
-            Guid flightGuid = Guid.Parse("42fdf6f2-0bff-4ee6-3ec7-08da3968162b");
+        [TestMethod]
+        public void updateEntity()
+        {
+            var seatService = new SeatService(seatRepoMock.Object);
 
             Seat seat = new Seat()
             {
-                Id = Guid.Parse("668e7f08-260a-4d8f-1fd9-08da3968163c"),
-                Number = 1,
-                isAvailable = true,
-                FlightId = Guid.Parse("42fdf6f2-0bff-4ee6-3ec7-08da3968162b")
+                Id = existingSeatId,
+                FlightId = existingFlightId,
             };
 
             seatService.UpdateFromEntity(seat);
+            seatService.SaveAsync();
 
-            var specificSeat = seatService.GetByCondition(seat => seat.Id == seatGuid && seat.FlightId == flightGuid);
+            var updateSeat = seatService.GetByCondition(seat => seat.Id == existingSeatId);
 
-            foreach(Seat updatedSeat in specificSeat)
+            foreach (Seat updatedSeat in updateSeat)
             {
-                Assert.IsTrue(updatedSeat.isAvailable);
+                Assert.AreEqual(existingSeatId, seat.Id);
             }
+
         }
+
+
+        //[TestMethod]
+        //public void UpdateSpecificSeat()
+        //{
+        //    SeatService seatService = init();
+
+        //    Guid seatGuid = Guid.Parse("668e7f08-260a-4d8f-1fd9-08da3968163c");
+        //    Guid flightGuid = Guid.Parse("42fdf6f2-0bff-4ee6-3ec7-08da3968162b");
+
+        //    Seat seat = new Seat()
+        //    {
+        //        Id = Guid.Parse("668e7f08-260a-4d8f-1fd9-08da3968163c"),
+        //        Number = 1,
+        //        isAvailable = true,
+        //        FlightId = Guid.Parse("42fdf6f2-0bff-4ee6-3ec7-08da3968162b")
+        //    };
+
+        //    seatService.UpdateFromEntity(seat);
+
+        //    var specificSeat = seatService.GetByCondition(seat => seat.Id == seatGuid && seat.FlightId == flightGuid);
+
+        //    foreach(Seat updatedSeat in specificSeat)
+        //    {
+        //        Assert.IsTrue(updatedSeat.isAvailable);
+        //    }
+        //}
     }
 }
